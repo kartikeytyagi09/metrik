663ad6604d26ae38ef3d6c1a22746eaaa8adce49
kartikey tyagi <tkartikey92@gmail.com>
2025-11-16 20:32:01 +0530

    navbar

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,8 @@
 import Link from "next/link";
 import Image from "next/image";
-import NavItems from "@/components/NavItems";
-import UserDropdown from "@/components/UserDropdown";
-import {searchStocks} from "@/lib/actions/finnhub.actions";
+import NavItems from "./NavItems";
 
-const Header = async ({ user }: { user: User }) => {
-    const initialStocks = await searchStocks();
+const Header = async () => {
 
     return (
         <header className="sticky top-0 header">
@@ -14,10 +11,8 @@ const Header = async ({ user }: { user: User }) => {
                     <Image src="/assets/icons/logo.svg" alt="Signalist logo" width={140} height={32} className="h-8 w-auto cursor-pointer" />
                 </Link>
                 <nav className="hidden sm:block">
-                    <NavItems initialStocks={initialStocks} />
+                    <NavItems />
                 </nav>
-
-                <UserDropdown user={user} initialStocks={initialStocks} />
             </div>
         </header>
     )
