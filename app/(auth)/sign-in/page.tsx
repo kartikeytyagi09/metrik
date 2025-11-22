0a8ed55c347d67953f3e4a03dcbbbab0d3346e6d
kartikey tyagi <tkartikey92@gmail.com>
2025-11-23 00:06:06 +0530

    previous cleanup

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -5,6 +5,7 @@ import { Button } from '@/components/ui/button';
 import InputField from '@/components/forms/InputField';
 import FooterLink from '@/components/forms/FooterLink';
 import {toast} from "sonner";
+import {signInEmail} from "better-auth/api";
 import {useRouter} from "next/navigation";
 import { signInWithEmail } from '@/lib/actions/auth.actions';
 
--- components/UserDropdown.tsx
+++ components/UserDropdown.tsx
@@ -15,7 +15,7 @@ import {LogOut} from "lucide-react";
 import NavItems from "@/components/NavItems";
 import { signOut } from "@/lib/actions/auth.actions";
 
-const UserDropdown = ({ user }: { user?: User }) => {
+const UserDropdown = ({ user }: { user?: User }) => {   
     const router = useRouter();
     console.log(user?.name);
 
@@ -47,15 +47,15 @@ const UserDropdown = ({ user }: { user?: User }) => {
             <DropdownMenuContent className="text-gray-400">
                 <DropdownMenuLabel>
                     <div className="flex relative items-center gap-3 py-2">
-                        <Avatar className="h-10 w-10">
+                        {/* <Avatar className="h-10 w-10">
                             <AvatarImage  />
                             <AvatarFallback className="bg-yellow-500 text-yellow-900 text-sm font-bold">
                                 {user?.name?.[0]?.toUpperCase()}
                             </AvatarFallback>
-                        </Avatar>
+                        </Avatar> */}
                         <div className="flex flex-col">
                             <span className='text-base font-medium text-gray-400'>
-                                 {user?.name }
+                                 {user?.email }
                             </span>
                         </div>
                     </div>
