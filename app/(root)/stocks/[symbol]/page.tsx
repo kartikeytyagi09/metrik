6e4f0d82e433e64c5093eafad431708692ed1859
kartikey tyagi <tkartikey92@gmail.com>
2025-12-02 00:17:47 +0530

    margins of search aap page

--- app/(root)/stocks/[symbol]/page.tsx
+++ app/(root)/stocks/[symbol]/page.tsx
@@ -15,7 +15,7 @@ export default async function StockDetails({ params }: StockDetailsPageProps) {
   const scriptUrl = `https://s3.tradingview.com/external-embedding/embed-widget-`;
 
   return (
-    <div className="flex min-h-screen p-4 md:p-6 lg:p-8">
+    <div className="flex min-h-screen p-4 md:p-1 lg:p-0">
       <section className="grid grid-cols-1 md:grid-cols-2 gap-8 w-full">
         {/* Left column */}
         <div className="flex flex-col gap-6">
